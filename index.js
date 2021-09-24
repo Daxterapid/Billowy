15300fb9aaa629e217948c2271706abaff9b6ff8
Daxterapid <craftonlymine@gmail.com>
2021-09-23 19:25:20 -0600

    update

---
+++ index.js
@@ -0,0 +1,52 @@
+async function handleTableSubmit(event) {
+  event.preventDefault();
+
+  const file = event.target.elements['table-upload'].files[0];
+  if (!file) {
+    uploadError('Please select a file.');
+    return false;
+  };
+
+  readFileJSON(file)
+    .then(response => {
+      const table = JSON.parse(response);
+
+      if (Object.keys(table).length === 0) {
+        uploadError('Data is empty.');
+        return;
+      }
+
+      $('#upload-error').attr('hidden', true);
+      $('#error-details').attr('hidden', true);
+
+      loadTable(table);
+    })
+    .catch(error => {
+      uploadError('An error occured while reading the file.');
+
+      $('#error-details').attr('hidden', false)
+      $('#error-text').text(error.name + ': ' + error.message);
+    });
+}
+
+function uploadError(message) {
+  $('#upload-error').attr('hidden', false);
+  $('#upload-error').text(message);
+}
+
+function readFileJSON(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = () => reject(reader.error);
+
+    reader.readAsText(file);
+  });
+}
+
+function loadTable(object) {
+  document.createElement('table');
+
+  
+}
